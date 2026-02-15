49f46eba4fce810a46b0492eeb4ef6956cce8fee
maxborey <maxborey@gmail.com>
2026-02-16 01:07:34 +0200

    feat: update header component with logo and navigation, enhance styles, and add base styles

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,5 +1,25 @@
 export const Header = () => {
   return `<div class="Header">
-  <div class="container">Hello</div>
-  </div>`;
+    <div class="container">
+        <div class="header__content_box">
+            <div class="logo">
+                <div class="logo__icon">
+                    <svg class="logo__icon-svg-blue" aria-hidden="true">
+                        <use href="/sprite.svg#icon-Vector_blue"></use>
+                    </svg>
+                    <svg class="logo__icon-svg" aria-hidden="true">
+                        <use href="/sprite.svg#icon-ukraine"></use>
+                    </svg>
+                </div>
+                <div class="logo__text">LearnLingo</div>
+            </div>
+            <div class="nav__box">
+                <nav class="nav">
+                    <a href="#" class="nav__link">Home</a>
+                    <a href="#" class="nav__link">Teachers</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>`;
 };
